e89b542309805105022c14d153736cd2fe8e16fc
LarkaFenrir <martina-colombo@hotmail.com>
2026-07-03 16:44:23 +0200

    Implement data validation and normalization
    Add data validation and normalization to serializers, checking the address is valid with the library i18naddress of Google.

---
+++ tracker/serializers.py
@@ -0,0 +1,88 @@
+from rest_framework import serializers
+from rest_framework_gis.serializers import GeoFeatureModelSerializer
+from models import Facility, Company, Violation
+from i18naddress import normalize_address, InvalidAddressError
+from sanitizers import strip_all_strings, clean_website_url, upper_country_code
+
+class FacilitySerializer(GeoFeatureModelSerializer):
+
+    class Meta:
+        model = Facility
+        geo_field = 'coordinates'
+        fields = [
+            'external_id',
+            'name',
+            'operator',
+            'city',
+            'country_code',
+            'postal_code',
+            'street',
+            'street_number',
+            'coordinates'
+        ]
+
+    def validate(self, data):
+        '''
+        Sanitize the input data and use Google's i18n database
+        to validate the address components.
+        '''
+        # combine street name and number to validate,
+        # but keep it separate for clean data structure
+        data = strip_all_strings(data)
+        if 'country_code' in data:
+            data['country_code'] = upper_country_code(data['country_code'])
+        full_address = f"{data.get('street')} {data.get('street_number')}"
+        address_data = {
+            'city': data.get('city'),
+            'country_code': data.get('country_code'),
+            'postal_code': data.get('postal_code'),
+            'street_address': full_address
+        }
+
+        try:
+            # after validating the street address it doesn't overwrite it
+            normalized_data = normalize_address(address_data)
+            data['city'] = normalized_data.get('city')
+            data['country_code'] = normalized_data.get('country_code')
+            data['postal_code'] = normalized_data.get('postal_code')
+
+        except InvalidAddressError as e:
+            errors = {}
+            for field, error_code in e.errors.items():
+                errors[field] = f'Invalid {field} for the selected country.'
+            raise serializers.ValidationError(errors)
+
+        return data
+
+class CompanySerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Company
+        fields = [
+            'name',
+            'website'
+        ]
+
+    def validate_website(self, data):
+        pass
+
+class ViolationSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Violation
+        fields = [
+            'facility',
+            'date_observed',
+            'description',
+            'evidence_url',
+            'evidence_file'
+        ]
+
+    def validate_date_observed(self, data):
+        pass
+
+    def validate_evidence_url(self, data):
+        pass
+
+    def validate_evidence_file(self, data):
+        pass
