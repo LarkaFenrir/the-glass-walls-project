bf29565655d21cf8dcdde146b5514c0deea0ea56
LarkaFenrir <martina-colombo@hotmail.com>
2026-07-03 15:07:50 +0200

    Add upper_country_code function
    Make sure that country code is in upper case, if it is a string.

--- tracker/sanitizers.py
+++ tracker/sanitizers.py
@@ -25,3 +25,11 @@ def clean_website_url(url):
     # if the url is None or an empty string, it doesn't strip it
     # avoid getting an error
     return url.strip() if url else url
+
+def upper_country_code(country_code):
+    '''
+    Ensure the country code is uppercase if it is a valid string.
+    '''
+    if isinstance(country_code, str):
+        return country_code.upper()
+    return country_code
