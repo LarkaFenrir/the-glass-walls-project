2219b15458917f152c7a9fc11bffbbf08efe12d1
LarkaFenrir <martina-colombo@hotmail.com>
2026-07-08 17:40:38 +0200

    Add features and fixes to serializers
    Add features and fixes to 'tracker/serializers.py':
    - override a built-in Django REST framework to validate an URL that does not have a protocol prefix in the website field of company,
    and in the evidence_url of violation
    - allow operator field in facility and facility field in violation to be null for their creation when no arg is passed
    - group all models into a single pipeline to test the integration flow

--- tracker/serializers.py
+++ tracker/serializers.py
@@ -1,9 +1,33 @@
 from rest_framework import serializers
 from rest_framework_gis.serializers import GeoFeatureModelSerializer
-from models import Facility, Company, Violation
+from tracker.models import Facility, Company, Violation
 from i18naddress import normalize_address, InvalidAddressError
-from sanitizers import strip_all_strings, clean_website_url, upper_country_code
+from tracker.sanitizers import strip_all_strings, clean_website_url, upper_country_code
 from django.utils import timezone
+
+class CompanySerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Company
+        fields = [
+            'name',
+            'website'
+        ]
+
+    def to_internal_value(self, data):
+        '''
+        It overrides a built-in Django REST framework to validate an URL that
+        does not have a protocol prefix.
+        '''
+        # the QueryDict that from the HTTP request has a copy attr
+        # the else statement is for my tests
+        data = data.copy() if hasattr(data, 'copy') else data
+        # if there is the 'website' key and it's not empty,
+        # it cleans the url
+        if 'website' in data and data['website']:
+            data['website'] = clean_website_url(data['website'])
+        # it validates with the built-in method of Django-REST framework again
+        return super().to_internal_value(data)
 
 class FacilitySerializer(GeoFeatureModelSerializer):
 
@@ -21,6 +45,10 @@ class FacilitySerializer(GeoFeatureModelSerializer):
             'street_number',
             'coordinates'
         ]
+
+        extra_kwargs = {
+            'operator': {'required': False, 'allow_null': True}
+        }
 
     def validate(self, data):
         '''
@@ -55,18 +83,6 @@ class FacilitySerializer(GeoFeatureModelSerializer):
 
         return data
 
-class CompanySerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = Company
-        fields = [
-            'name',
-            'website'
-        ]
-
-    def validate_website(self, value):
-        return clean_website_url(value)
-
 class ViolationSerializer(serializers.ModelSerializer):
 
     class Meta:
@@ -79,10 +95,37 @@ class ViolationSerializer(serializers.ModelSerializer):
             'evidence_file'
         ]
 
+        extra_kwargs = {
+            'facility': {'required': False, 'allow_null': True}
+        }
+
     def validate_date_observed(self, value):
         if value > timezone.now().date():
             raise serializers.ValidationError("The date cannot be in the future.")
         return value
 
-    def validate_evidence_url(self, value):
-        return clean_website_url(value)
+    def to_internal_value(self, data):
+        data = data.copy() if hasattr(data, 'copy') else data
+        if 'evidence_url' in data and data['evidence_url']:
+            data['evidence_url'] = clean_website_url(data['evidence_url'])
+        return super().to_internal_value(data)
+
+class TrackerSerializer(serializers.Serializer):
+
+    company = CompanySerializer()
+    facility = FacilitySerializer()
+    violation = ViolationSerializer(required=False, allow_null=True)
+
+    def create(self, validated_data):
+        company_data = validated_data.pop('company')
+        facility_data = validated_data.pop('facility')
+        violation_data = validated_data.pop('violation', None)
+
+        company = Company.objects.create(**company_data)
+        facility = Facility.objects.create(operator=company, **facility_data)
+
+        violation = None   # in case no violation is given, no error thrown
+        if violation_data:
+            violation = Violation.objects.create(facility=facility, **violation_data)
+
+        return violation
