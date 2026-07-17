a5e93c23f870a11c6675075bbdbd1f8b621d0ad4
LarkaFenrir <martina-colombo@hotmail.com>
2026-07-17 15:21:44 +0200

    Add paths for 'config/urls'
    Add tracker and token path to 'config/urls'

--- config/urls.py
+++ config/urls.py
@@ -18,10 +18,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('tracker.urls'))
+    path('api/', include('tracker.urls', namespace='tracker')),
+    path('api_token_auth', obtain_auth_token, name='api-token-auth')
 ]
 
 if settings.DEBUG:
