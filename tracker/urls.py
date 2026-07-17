e7d5b825a70075af923e201da6914b8b215014a8
LarkaFenrir <martina-colombo@hotmail.com>
2026-07-17 16:29:54 +0200

    Add url paths in tracker app

--- tracker/urls.py
+++ tracker/urls.py
@@ -14,7 +14,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from .views import FacilityList, FacilityDetail
 
-urlpatterns = []
+app_name = 'tracker'
+urlpatterns = [
+    path('facility/', FacilityList.as_view(), name='facility-list'),
+    path('facility/<int:pk>', FacilityDetail.as_view(), name='facility-detail')
+]
