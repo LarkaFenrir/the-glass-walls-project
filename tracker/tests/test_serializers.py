089c74511c46c17f15d4c7f50e473e756716123e
LarkaFenrir <martina-colombo@hotmail.com>
2026-07-21 12:25:39 +0200

    Add full field assertions to test_serializers

--- tracker/tests/test_serializers.py
+++ tracker/tests/test_serializers.py
@@ -57,6 +57,15 @@ class TrackerSerializerTest(TestCase):
         clean_data = FacilitySerializer(data=messy_data)
         self.assertTrue(clean_data.is_valid(), clean_data.errors)
         self.assertEqual(clean_data.validated_data['external_id'], 'way/29177094')
+        self.assertEqual(clean_data.validated_data['name'], 'Schlachthof Gärtringen')
+        self.assertEqual(clean_data.validated_data['operator'], self.company)
+        self.assertEqual(clean_data.validated_data['city'], 'Gärtringen')
+        self.assertEqual(clean_data.validated_data['country_code'], 'DE')
+        self.assertEqual(clean_data.validated_data['postal_code'], '12345')
+        self.assertEqual(clean_data.validated_data['street'], 'Riedbrunnenstraße')
+        self.assertEqual(clean_data.validated_data['street_number'], '5')
+        self.assertEqual(clean_data.validated_data['coordinates'].x, 8.9190879)
+        self.assertEqual(clean_data.validated_data['coordinates'].y, 48.6396181)
 
     def test_facility_serializer_invalid(self):
         messy_data = {
@@ -120,22 +129,30 @@ class TrackerSerializerTest(TestCase):
                 'street': 'Riedbrunnenstraße',
                 'street_number': '5',
                 'coordinates': 'SRID=4326;POINT(8.9190879 48.6396181)'
-            },
-
-            'violation': {
-                'date_observed': timezone.now().date(),
-                'description': 'Failed stunning equipment.'
             }
         }
         clean_data = TrackerSerializer(data=messy_data)
         self.assertTrue(clean_data.is_valid(), clean_data.errors)
         # saving to trigger the function create()
-        slaughterhouse_violation = clean_data.save()
-        # check that everything is written correctly in the db
-        self.assertIsNotNone(slaughterhouse_violation.id)
-        self.assertEqual(
-            slaughterhouse_violation.facility.operator.name, 'Schlachthof eG'
-        )
+        created_objects = clean_data.save()
+        # extract instances
+        company = created_objects['company']
+        facility = created_objects['facility']
+        # check company is created and sanitized
+        self.assertIsInstance(company, Company)
+        self.assertEqual(company.name, 'Schlachthof eG')
+        self.assertEqual(company.website, 'https://companyweb.com')
+        # check facility is created and sanitized
+        self.assertIsInstance(facility, Facility)
+        self.assertEqual(facility.external_id, 'way/29177094')
+        self.assertEqual(facility.name, 'Schlachthof Gärtringen')
+        self.assertEqual(facility.city, 'Gärtringen')
+        self.assertEqual(facility.country_code, 'DE')
+        self.assertEqual(facility.postal_code, '12345')
+        self.assertEqual(facility.street, 'Riedbrunnenstraße')
+        self.assertEqual(facility.street_number, '5')
+        self.assertEqual(facility.coordinates.x, 8.9190879)
+        self.assertEqual(facility.coordinates.y, 48.6396181)
 
     def test_tracker_serializer_invalid(self):
         messy_data = {
@@ -154,11 +171,6 @@ class TrackerSerializerTest(TestCase):
                 'street': 'Riedbrunnenstraße',
                 'street_number': '5',
                 'coordinates': 'SRID=4326;POINT(8.9190879 48.6396181)'
-            },
-
-            'violation': {
-                'date_observed': timezone.now().date(),
-                'description': 'Failed stunning equipment.'
             }
         }
         clean_data = TrackerSerializer(data=messy_data)
