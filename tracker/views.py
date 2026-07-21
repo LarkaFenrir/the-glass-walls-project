81458b308b1ffac1f446542f463f145c4dcf940f
LarkaFenrir <martina-colombo@hotmail.com>
2026-07-21 13:13:14 +0200

    Add Company and Violation views

--- tracker/views.py
+++ tracker/views.py
@@ -1,12 +1,69 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from .models import Facility
-from .serializers import FacilitySerializer
+from .models import Company, Facility, Violation
+from .serializers import CompanySerializer, FacilitySerializer, ViolationSerializer
 from rest_framework import status
 from .permissions import IsAdminOrReadOnly
 from rest_framework.authentication import TokenAuthentication
 
 # Create your views here.
+
+class CompanyList(APIView):
+
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAdminOrReadOnly]
+
+    def get(self, request):
+        companies = Company.objects.all()
+        serializer = CompanySerializer(companies, many=True)
+        return Response(serializer.data)
+
+
+class CompanyDetail(APIView):
+
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAdminOrReadOnly]
+
+    def get_company(self, pk):
+        try:
+            return Company.objects.get(pk=pk)
+        except Company.DoesNotExist:
+            return None
+
+    def get(self, request, pk):
+        company = self.get_company(pk)
+        if company is None:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        serializer = CompanySerializer(company)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        company = self.get_company(pk)
+        if company is None:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        serializer = CompanySerializer(company, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def patch(self, request, pk):
+        company = self.get_company(pk)
+        if company is None:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        serializer = CompanySerializer(company, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        company = self.get_company(pk)
+        if company is None:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        company.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
 
 class FacilityList(APIView):
 
@@ -41,7 +98,6 @@ class FacilityDetail(APIView):
         facility = self.get_facility(pk)
         if facility is None:
             return Response(status=status.HTTP_404_NOT_FOUND)
-        self.check_object_permissions(obj=facility, request=request)
         serializer = FacilitySerializer(facility, data=request.data)
         if serializer.is_valid():
             serializer.save()
@@ -52,7 +108,6 @@ class FacilityDetail(APIView):
         facility = self.get_facility(pk)
         if facility is None:
             return Response(status=status.HTTP_404_NOT_FOUND)
-        self.check_object_permissions(obj=facility, request=request)
         serializer = FacilitySerializer(facility, data=request.data, partial=True)
         if serializer.is_valid():
             serializer.save()
@@ -63,6 +118,61 @@ class FacilityDetail(APIView):
         facility = self.get_facility(pk)
         if facility is None:
             return Response(status=status.HTTP_404_NOT_FOUND)
-        self.check_object_permissions(obj=facility, request=request)
         facility.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+class ViolationList(APIView):
+
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAdminOrReadOnly]
+
+    def get(self, request):
+        violations = Violation.objects.all()
+        serializer = ViolationSerializer(violations, many=True)
+        return Response(serializer.data)
+
+
+class ViolationDetail(APIView):
+
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAdminOrReadOnly]
+
+    def get_violation(self, pk):
+        try:
+            return Violation.objects.get(pk=pk)
+        except Violation.DoesNotExist:
+            return None
+
+    def get(self, request, pk):
+        violation = self.get_violation(pk)
+        if violation is None:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        serializer = ViolationSerializer(violation)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        violation = self.get_violation(pk)
+        if violation is None:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        serializer = ViolationSerializer(violation, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def patch(self, request, pk):
+        violation = self.get_violation(pk)
+        if violation is None:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        serializer = ViolationSerializer(violation, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        violation = self.get_violation(pk)
+        if violation is None:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        violation.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
